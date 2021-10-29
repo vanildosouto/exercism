58b62eaa6f4a4dce8aa592b6fce0f8318061cae4
Vanildo Souto Mangueira <vanildo.souto@gmail.com>
2021-10-29 11:17:46 -0300

    adiciona exercicio difference-of-squares em GO

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,23 @@
+package diffsquares
+
+func SquareOfSum(n int) int {
+	sum := 0
+	for x := 1; x <= n; x++ {
+		sum += x
+	}
+
+	return sum * sum
+}
+
+func SumOfSquares(n int) int {
+	sum := 0
+	for x := 1; x <= n; x++ {
+		sum += x * x
+	}
+
+	return sum
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
